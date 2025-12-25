329fa4b79926aed0b34f78a4bc68d883ef6320c5
ILTH100Y <565301251@qq.com>
2025-12-25 12:20:05 +0800

    Update 4.1.4.cpp

--- 4.1.4.cpp
+++ 4.1.4.cpp
@@ -7,7 +7,7 @@ int main()
 	cout << "Enter list1:";
 	int x, y;
 	cin >> x;
-	int a[80],b[80],c[80];
+	int a[80],b[80],c[160];
 	for (int i = 0; i < x; i++)
 		cin >> a[i];
 	cout << "Enter list2:";
@@ -21,19 +21,34 @@ int main()
 }
 void merge(const int list1[], int size1, const int list2[], int size2, int list3[])
 {
-	for (int i = 0; i < size1; i++)
-		list3[i] = list1[i];
-	for (int i = 0; i < size2; i++)
-		list3[i + size1] = list1[i];
-	bool changed = true;
-	do
-	{
-		changed = false;
-		for (int j = 0; j < size1+size2-1; j++)
-			if (list3[j] > list3[j + 1])
-			{
-				swap (list3[j] ,list3[j + 1]);
-				changed = true;
-			}
-	} while (changed);
+    int i = 0;  // list1的索引
+    int j = 0;  // list2的索引
+    int k = 0;  // list3的索引
+    int comparisons = 0;  // 比较次数计数
+
+    // 合并两个有序数组
+    while (i < size1 && j < size2)
+    {
+        comparisons++;  // 每次比较计数
+        if (list1[i] <= list2[j])
+        {
+            list3[k++] = list1[i++];
+        }
+        else
+        {
+            list3[k++] = list2[j++];
+        }
+    }
+
+    // 复制list1剩余的元素
+    while (i < size1)
+    {
+        list3[k++] = list1[i++];
+    }
+
+    // 复制list2剩余的元素
+    while (j < size2)
+    {
+        list3[k++] = list2[j++];
+    }
 }
