f21aa43d01a056912e517b246ff2b84c1c79264c
ILTH100Y <565301251@qq.com>
2025-12-25 11:26:06 +0800

    Update 3.3.cpp

--- 3.3.cpp
+++ 3.3.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	double cel1 = 40.0, cel2 = 39.0, cel3 = 31.0, fah1 = 120.0, fah2 = 110.1, fah3 = 30.0;
+	double cel1 = 40.0, cel2 = 39.0, cel3 = 31.0, fah1 = 120.0, fah2 = 110.0, fah3 = 30.0;
 	cout << "Celsius" << '\t' << "Fahrenheit" << "|" << "Fahrenheit" << '\t' << "Celsius" << endl;
 	cout << cel1 << '\t' << celsius_to_fah(cel1) << "|" << fah1 << '\t' << fahrenheit_to_cels(fah1) << endl;
 	cout << cel2 << '\t' << celsius_to_fah(cel2) << "|" << fah2 << '\t' << fahrenheit_to_cels(fah2) << endl;
@@ -11,5 +11,6 @@ int main()
 	cout << cel3 << '\t' << celsius_to_fah(cel3) << "|" << fah3 << '\t' << fahrenheit_to_cels(fah3) << endl;
 	return 0;
 }
+
 
 
