e8e9834f49c9bff931f75ee7ae460dec86fcbd22
ILTH100Y <565301251@qq.com>
2025-12-25 10:13:57 +0800

    3.1.cpp

--- 3.1.cpp
+++ 3.1.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 using namespace std;
+int maxy(int  num1, int num2)
+{
+	int n = (num1 < num2) ? num1 : num2;
+	for (n; n > 0; n--)
+		if ((num1 % n == 0) && (num2 % n == 0))
+			break;
+	return n;
+}
+int minb(int  num1, int num2)
+{
+	int n = (num1 > num2) ? num1 : num2;
+	for (n;; n++)
+		if ((n % num1  == 0) && (n % num2 == 0))
+			break;
+	return n;
+}
 int main()
 {
-	int a[10];
-	cout << "请输入十个数字：";
-	for (int i = 0; i < 10; i++)
-		cin >> a[i];
-	int b[10]={0}, count = 0;
-	for (int i = 0; i < 10; i++)
-	{
-		bool repeat = false;
-		for (int j = 0; j <= count; j++)
-		
-			if (a[i] == b[j])
-			{
-				repeat = true;
-				break;
-			}
-		if (!repeat)
-		{
-			b[count] = a[i];
-			count++;
-		}
-	}
-	cout << "不同的数字为：";
-	for (int i = 0; i < count; i++)
-		cout << b[i];
+	int m, n;
+	cout << "请输入两个数字：";
+	cin >> m>> n;
+	cout << "它们的最大公因数为：" << maxy(m, n) << endl;
+	cout << "它们的最小公倍数为：" << minb(m, n) << endl;
 	return 0;
 }
