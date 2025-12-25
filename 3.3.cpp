3ee9a05b0a459878aa540f16b3ea9f2adcc341b5
ILTH100Y <565301251@qq.com>
2025-12-25 11:25:52 +0800

    Update 3.3.cpp

--- 3.3.cpp
+++ 3.3.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <mytemperature>
+#include "mytemperature.h"
 using namespace std;
 int main()
 {
@@ -11,4 +11,5 @@ int main()
 	cout << cel3 << '\t' << celsius_to_fah(cel3) << "|" << fah3 << '\t' << fahrenheit_to_cels(fah3) << endl;
 	return 0;
 }
+
 
